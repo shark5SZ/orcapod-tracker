887c7964564adb50d4ad08c4ab3978d10eebb73b
Adiele Hope <dessertant@gmail.com>
2025-09-23 17:57:27 -0700

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Orcapod Tracker</h1>
+      <p>Track and analyse your social reach, one status at a time</p>
+    </>
+  )
+}
+
+export default App
